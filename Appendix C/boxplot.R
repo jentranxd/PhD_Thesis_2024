4862eb732d60d49a6c19d46fc87e87d6f6e7fe68
Jennifer Tran <jstran@wisc.edu>
2024-08-21 02:06:51 -0500

    Update boxplot.R

--- Appendix C/boxplot.R
+++ Appendix C/boxplot.R
@@ -28,4 +28,35 @@ ggplot(atp, aes(x = strain, y = relative_luminescence)) +
   theme(axis.text.x = element_text(angle = 0, hjust = 0.5, vjust = 0.5))
 
 ####################
+#load ATP assay data
 
+nad <- fread("./nad_nadh_assay.csv", header = TRUE)
+
+# Subtract the corresponding replicate blanks from all the values
+nad_adjusted <- nad %>%
+  group_by(replicate) %>%
+  mutate(lum_NAD = lum_NAD - lum_NAD[strain == "blank"],
+         lum_NADH = lum_NADH - lum_NADH[strain == "blank"]) %>%
+  ungroup()
+
+# Calculate NAD/NADH ratio
+nad_adjusted <- nad_adjusted %>%
+  mutate(ratio = lum_NAD / lum_NADH)
+
+# Set the order of strains
+nad_adjusted$strain <- factor(nad_adjusted$strain, levels = c("non-targeting", "nuoB", "sdhB", "cyoA", "atpB"))
+
+# Filter out the blank rows
+nad_adjusted <- nad_adjusted %>%
+  filter(strain != "blank")
+
+# Plot the boxplots  (n=3 so the boxplot is literally just the 3 points)
+ggplot(nad_adjusted, aes(x = strain, y = ratio)) +
+  geom_boxplot(outlier.shape = NA) +
+  labs(title = "NAD+/NADH Ratio of Strains",
+       x = "Strain",
+       y = "NAD+/NADH Ratio") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 0, hjust = 0.5, vjust = 0.5))
+
+####################
